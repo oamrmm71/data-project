6f64744256585d69f738f5af02d6a6a222f3f452
oamrmm71 <164521821+oamrmm71@users.noreply.github.com>
2024-05-17 14:38:39 +0300

    Update FileName.cpp

--- FileName.cpp
+++ FileName.cpp
@@ -1,1 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+
+
+class huffmantree {
+	char data;
+	int freq;
+
+	huffmantree* left;
+	huffmantree* right;
+	huffmantree(char character,int frequency)
+	{
+		data = character;
+		freq = frequency;
+		left = right = NULL;
+	}
+};
+huffmantree* buildhuffmantree(int freq[], int top) {
+	Node* head = NULL;
+
+	for (int i = 0; i < ASCII_SIZE; i++) {
+		if (freq[i].freq > 0) {
+			insertNode(&head, createNode(freq[i].ch, freq[i].freq));
+		}
+	}
+
+	while (head != NULL && head->next != NULL) {
+		Node* left = head;
+		Node* right = head->next;
+		head = right->next;
+
+		top = newNode('$', left->freq + right->freq);
+		top->left = left;
+		top->right = right;
+
+		insertNode(&head, top);
+	}
+
+	return head;
+}
+
+void findfreq(char s[])
+{
+	int i = 0;
+
+	int freq[26] = { 0 };
+
+	while (s[i] != '\0')
+	{
+		freq[s[i] - 'a']++;
+		i++;
+	}
+	printfreq(freq);
+}
 
